c6723ae0b37ca82d1cf7091ed80561583c98fe71
Ana Pilar <pilarbarba03@gmail.com>
2022-12-15 16:53:23 +0100

    2) Se ha realizado la clase Destinatario con la relacion ya creada, la clase caja está vacía, solamente era para que no diese error en la relacion

---
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/model/Destinatario.java
@@ -0,0 +1,35 @@
+package com.salesianostriana.dam.kiloapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Destinatario {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre;
+
+    private String direccion;
+
+    private String personaContacto;
+
+    private String telefono;
+
+    @OneToMany(mappedBy = "destinatario",
+            orphanRemoval = true,
+            cascade = CascadeType.REMOVE,
+            fetch = FetchType.EAGER)
+    private List<Caja> cajaList = new ArrayList<>();
+}
