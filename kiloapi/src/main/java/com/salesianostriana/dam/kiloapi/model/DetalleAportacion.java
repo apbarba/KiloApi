2e174b7edf44aaf4799237335688b64f50336812
Jose Ignacio <rivas.fujos20@triana.salesianos.edu>
2022-12-15 18:38:01 +0100

    Agrego entidades

---
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/model/DetalleAportacion.java
@@ -0,0 +1,33 @@
+package com.salesianostriana.dam.kiloapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class DetalleAportacion {
+
+    @EmbeddedId
+    private DetallesPK detallesPK;
+
+    //    @MapsId("numLinea_id")
+    private int numLinea;
+
+    private double cantidadKg;
+
+//    @ManyToOne
+//    @MapsId("id_aportacion")
+//    @JoinColumn(name = "aportacion_id",
+//            foreignKey = @ForeignKey(name = "FK_DETALLE_APORTACION_APORTACION"))
+//    private Aportacion aportacion;
+
+//    @ManyToOne
+//    @JoinColumn(name = "tipoAlimento_id",
+//            foreignKey = @ForeignKey(name = "FK_DETALLE_TIPO_ALIMENTO"))
+//    private TipoAlimento tipoAlimento;
+}
