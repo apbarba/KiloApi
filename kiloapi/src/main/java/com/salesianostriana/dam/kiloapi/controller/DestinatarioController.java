ee34dfa8b2237c503f9ceb15fb3d78fd7585b0b4
Arturo Céspedes Pedrazas <cespedes.peart22@salesianos.trian.edu>
2022-12-16 08:27:21 +0100

    deleteDestinatario realizado

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/controller/DestinatarioController.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/controller/DestinatarioController.java
@@ -39,26 +39,7 @@ public class DestinatarioController {
         Optional<Destinatario> d1 = destinatarioService.findById(id);
         return d1.isEmpty() ?
                 ResponseEntity.status(HttpStatus.NOT_FOUND).build()
-                    : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 
     }
-    @Operation(summary = "Obtiene la información de un destinatario por su ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "Se ha encontrado el destinatario relacionado con ese ID",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Destinatario.class))}),
-            @ApiResponse(responseCode = "404",
-                    description = "No se ha encontrado un destinatario con ese ID",
-                    content = @Content),
-    })
-    @GetMapping("/destinatario/{id}")
-    public ResponseEntity<Destinatario> findById(@PathVariable Long id) {
-        Optional<Destinatario> d1 = destinatarioService.findById(id);
-        return d1.isEmpty() ?
-                ResponseEntity.status(HttpStatus.NOT_FOUND).build()
-                    : ResponseEntity.of(destinatarioService.findById(id));
-
-    }
-
 }
--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repository/AportacionRepository.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repository/AportacionRepository.java
@@ -1,6 +1,5 @@
 package com.salesianostriana.dam.kiloapi.repository;
 
-import com.salesianostriana.dam.kiloapi.controller.AportacionController;
 import com.salesianostriana.dam.kiloapi.model.Aportacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
