7ce980ba88302c1a76d532bf5f99ea4ca33d973b
Ana Pilar Barba López <pilarbarba03@gmail.com>
2022-12-19 10:06:36 +0100

    nada

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repository/ClaseRepository.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/repository/ClaseRepository.java
@@ -1,7 +1,15 @@
 package com.salesianostriana.dam.kiloapi.repository;
 
+import com.salesianostriana.dam.kiloapi.model.Aportacion;
 import com.salesianostriana.dam.kiloapi.model.Clase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClaseRepository extends JpaRepository<Clase, Long> {
+
+    //Consulta para traernos los kilos totales que ha aportado una clase(id)
+
+  
 }
