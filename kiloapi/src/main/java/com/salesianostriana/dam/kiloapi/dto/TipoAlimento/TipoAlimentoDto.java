87afd2f909f94a6273a98d663c762f1ba7d35ae2
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2022-12-19 09:21:05 +0100

    Corrección TipoAlimentoDto

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/TipoAlimento/TipoAlimentoDto.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/TipoAlimento/TipoAlimentoDto.java
@@ -2,6 +2,7 @@ package com.salesianostriana.dam.kiloapi.dto.TipoAlimento;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.salesianostriana.dam.kiloapi.dto.Caja.CajaViews;
 import com.salesianostriana.dam.kiloapi.model.Tiene;
 import com.salesianostriana.dam.kiloapi.model.TipoAlimento;
 import lombok.AllArgsConstructor;
@@ -16,13 +17,13 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TipoAlimentoDto {
 
-    @JsonView(TipoAlimentoViews.Master.class)
+    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class})
     private Long id;
-    @JsonView(TipoAlimentoViews.Master.class)
+    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class})
     private String nombre;
     @JsonView(TipoAlimentoViews.MostrarDisponible.class)
     private Double kilosDisponibles;
-    @JsonView(TipoAlimentoViews.MostrarEnviado.class)
+    @JsonView({TipoAlimentoViews.MostrarEnviado.class, CajaViews.Master.class})
     private double kilosEnviados;
 
 }
