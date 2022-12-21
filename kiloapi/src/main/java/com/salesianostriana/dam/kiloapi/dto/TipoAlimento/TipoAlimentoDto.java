4c6e8590f883ee8f81e5b827624b6ebd07387f8a
Mario Ruiz <mariorldam@gmail.com>
2022-12-21 16:29:22 +0100

    Corrección TipoAlimentoDto

--- kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/TipoAlimento/TipoAlimentoDto.java
+++ kiloapi/src/main/java/com/salesianostriana/dam/kiloapi/dto/TipoAlimento/TipoAlimentoDto.java
@@ -3,25 +3,21 @@ package com.salesianostriana.dam.kiloapi.dto.TipoAlimento;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.salesianostriana.dam.kiloapi.dto.Caja.CajaViews;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-
-//@AllArgsConstructor
-
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TipoAlimentoDto {
 
-    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class})
+    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class, CajaViews.PostCajaTipo.class})
     private Long id;
-    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class})
+    @JsonView({TipoAlimentoViews.Master.class, CajaViews.Master.class, CajaViews.PostCajaTipo.class})
     private String nombre;
-    @JsonView({TipoAlimentoViews.MostrarDisponible.class, CajaViews.Master.class})
+    @JsonView({TipoAlimentoViews.MostrarDisponible.class, CajaViews.Master.class, CajaViews.PostCajaTipo.class})
     private Double kilosDisponibles;
     @JsonView({TipoAlimentoViews.MostrarEnviado.class, CajaViews.Master.class})
     private Double kilosEnviados;
